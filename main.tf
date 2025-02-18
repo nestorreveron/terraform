c440cdc37a21fa39cf63d4e306151bfde95bb719
John Doe <johndoe@example.com>
2025-02-18 12:09:15 +0000

    created resource group

---
+++ main.tf
@@ -0,0 +1,20 @@
+# Specifiy the provider and version
+ terraform {
+   required_providers {
+     azurerm = {
+       source  = "hashicorp/azurerm"
+       version = "~> 4.0"
+     }
+   }
+ }
+    
+ # Configure the Microsoft Azure Provider
+ provider "azurerm" {
+   features {}
+ }
+    
+ # Create the very first resource
+ resource "azurerm_resource_group" "nestorterraform" {
+   name     = "nestorterraform"
+   location = "eastus2"
+ }
