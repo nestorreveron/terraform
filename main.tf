61879975ab93ceb08ee8b8834a49ee845a33478f
John Doe <johndoe@example.com>
2025-02-18 13:37:24 +0000

    added Var

--- main.tf
+++ main.tf
@@ -1,32 +1,41 @@
 terraform {
-   required_providers {
-     azurerm = {
-       source  = "hashicorp/azurerm"
-       version = "~> 4.0"
-     }
-   }
- }
-    
- provider "azurerm" {
-   features {}
- }
-    
- locals {
-   prefix = "nestorlab02"
-   region = "eastus2"
-   tags = {
-     cost_center = "contoso research"
-   }
- }
-    
- resource "azurerm_resource_group" "contoso_rg" {
-   name     = "${local.prefix}_rg"
-   location = local.region
-   tags     = local.tags
- }
-    
- resource "azurerm_resource_group" "contoso_dev_rg" {
-   name     = "${local.prefix}_dev_rg"
-   location = local.region
-   tags     = local.tags
- }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "prefix" {}
+
+variable "region" {
+  type        = string
+  default     = "East US 2"
+  description = "The Azure region to deploy resources"
+  validation {
+    condition     = contains(["UK South", "UK West", "North Europe", "West Europe", "East US 2", "West US"], var.region)
+    error_message = "Invalid region"
+  }
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "A map of tags"
+}
+
+resource "azurerm_resource_group" "contoso_rg" {
+  name     = "${var.prefix}_rg"
+  location = var.region
+  tags     = var.tags
+}
+
+resource "azurerm_resource_group" "contoso_dev_rg" {
+  name     = "${var.prefix}_dev_rg"
+  location = var.region
+  tags     = var.tags
+}
